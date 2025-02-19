19af6b704e432d4475857b282440c52664f19465
Partha <github@partha51613.com>
2025-02-19 22:52:11 +0530

    basic login page using AdminLTE4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: "login",
+        component: LoginComponent,
+    }
+];

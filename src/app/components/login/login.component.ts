2ccae65b00d45ee3e1f667d46e76a352552fda7e
Partha <github@partha51613.com>
2025-02-22 11:43:16 +0530

    Only email is there is login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   router = inject(Router)
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
+
+  formValue: any;
+
   onLogin() {
-    this.router.navigateByUrl('dashboard2')
+    // this.formValue = this.loginForm.value;
+    // console.log(this.formValue)
+    // console.log(this.formValue)
+    console.log(this.loginForm.value)
+
   }
+
 }

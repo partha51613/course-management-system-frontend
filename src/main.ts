92ca790b076f10c8c407a7f1e456f8ade770bfeb
Partha <github@partha51613.com>
2025-02-23 22:10:23 +0530

    added HttpClientModule to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers || [], // Keep existing providers
+    importProvidersFrom(HttpClientModule), // Add HttpClientModule here
+  ]
+}).catch((err) => console.error(err));

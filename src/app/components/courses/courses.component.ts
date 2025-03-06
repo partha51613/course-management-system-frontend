d9325ff8a98c716e7da3d8308d9371e9c18a52ee
Partha <github@partha51613.com>
2025-03-06 22:55:07 +0530

    in progress: working on adding a button/dropdown to fetch courses

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {MatTable, MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css',
   standalone: true
@@ -14,5 +14,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 export class CoursesComponent {
-  
+
+    //Injection
+    private ApiService = inject(ApiService);
+
+  getCourses(){
+    console.log('getCourses Clicked');
+
+    const ENDPOINT = "/courses"
+    this.ApiService.postData(ENDPOINT,null).subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
+
+  // getCourses(){
+  //   console.log("HI")
+  // }
 }

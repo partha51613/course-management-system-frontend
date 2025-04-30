61bdb987adc9758931df3854b1d1d725959d3516
Partha Choudhury <partha.choudhury@rnd.iitg.ac.in>
2025-04-30 12:25:36 +0530

    Added pagination to users page

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,24 +10,63 @@ import { ApiService } from '../../services/api.service';
 })
 export class UsersComponent {
 
-  usersData: any;
-  //Injection
-    private ApiService = inject(ApiService);
+  // Pagination variables
+  rowsPerPage = 5;
+  currentPage = 1;
+  offsetValue = 0;
+  isHasMoreData = true;
+  totalPages ?= 1;
 
+  // Button enable/disable state
+  isPreviousButtonDisabled = true;
+  isNextButtonDisabled = false;
+  
+  //Variables
+  usersData: any;
+
+  //Injection
+  private ApiService = inject(ApiService);
+
+  calcOnPageChange() {
+    this.offsetValue = (this.currentPage - 1) * this.rowsPerPage;
+    this.getUserData()
+  }
 
   ngOnInit(){
     this.getUserData();
   }
   getUserData(){
-    const ENDPOINT = "/users"
+    const ENDPOINT = `/users?limit=${this.rowsPerPage}&offset=${this.offsetValue}`;
     this.ApiService.getData(ENDPOINT).subscribe({
       next: (response) => {
-        console.log(response)
-        this.usersData = response.data;
+        console.log(response);
+        this.usersData = response.data ?? [];
+        
+        //
+        this.totalPages =  Math.ceil(response.totalCount / this.rowsPerPage);
+        this.isPreviousButtonDisabled = this.currentPage === 1;
+        this.isNextButtonDisabled = this.currentPage == this.totalPages;
+
       },
       error: (err) => {
-        console.log(err)
-      }
-    })
+        this.usersData = [];
+        console.log('Error fetching courses:', err);
+      },
+    });
+  }
+
+  gotoNextPage() {
+    // if (this.isNextButtonDisabled) return;
+    // Check if there's potentially another page
+    
+    this.currentPage++;
+    this.calcOnPageChange()
+  }
+
+  gotoPrevPage() {
+    // if (this.isPreviousButtonDisabled) return;
+    this.currentPage--;
+    this.calcOnPageChange()
+
   }
 }
